3d5d9a8252253549fe06c533ed81c0c9e392c0c2
Alex <aenderw@gmail.com>
2014-07-31 20:44:49 -0700

    Added Media (images only currently)
    When user requests and response contains media file (web link to image
    currently) it adds to to the list in “media” on the page.

--- ersim/response.py
+++ ersim/response.py
@@ -1,18 +1,27 @@
 import sqlite3
+import json
 
 def generateResponse(patientID, triggerValue):
-	response = "** Please ask a question (history, physical exam, etc.) with '?' [ex: 'pain?' will ask the patient if they have pain] or perform an action (medications, lab, imaging, etc.) with '.' [ex: '.cbc' will place an order for a CBC] **"
+	responseText = "** Please ask a question (history, physical exam, etc.) with '?' [ex: 'pain?' will ask the patient if they have pain] or perform an action (medications, lab, imaging, etc.) with '.' [ex: '.cbc' will place an order for a CBC] **"
+
+	responseMedia = ""
+
 	# if its a request for response (ends with ?)
 	if triggerValue.endswith('?'):
 		conn = sqlite3.connect('extra/test.sqlite')
 		c = conn.cursor()
-		c.execute('SELECT response FROM responses, tr_links, ptr_links, triggers WHERE responses.id=tr_links.response_id AND tr_links.id=ptr_links.tr_link_id AND ptr_links.patient_id=? AND tr_links.trigger_id=triggers.id AND triggers.trigger=?', (patientID, triggerValue[:-1]))
-		response = c.fetchone()
+		c.execute('SELECT response, media_id FROM responses, tr_links, ptr_links, triggers WHERE responses.id=tr_links.response_id AND tr_links.id=ptr_links.tr_link_id AND ptr_links.patient_id=? AND tr_links.trigger_id=triggers.id AND triggers.trigger=?', (patientID, triggerValue[:-1]))
+		result = c.fetchone()
 
-		if response == None:
-			response = "I do not want to answer that."
+		if result == None:
+			responseText = "I do not want to answer that."
 		else:
-			response = response[0]
+			responseText = result[0]
+
+			c.execute('SELECT file FROM media WHERE media.id=?', (result[1],))
+			result = c.fetchone()
+			if result != None:
+				responseMedia = result[0]
 
 		conn.close()
 
@@ -33,12 +42,14 @@ def generateResponse(patientID, triggerValue):
 			if tempUserAction == None:
 				c.execute('INSERT INTO user_actions (user_id, action_id, patient_id, timestamp) VALUES (?,?,?,CURRENT_TIMESTAMP)', (1, tempActionID, 1))
 				conn.commit()
-				response = "* Done. *"
+				responseText = "* Done. *"
 			else:
-				response = "* You already did that! *"
+				responseText = "* You already did that! *"
 		else:
-			response = "* Could not perform that action, not available. *"
+			responseText = "* Could not perform that action, not available. *"
 
 		conn.close()
 
-	return response
+	response = {"text":responseText, "media":responseMedia}
+
+	return json.dumps(response)
